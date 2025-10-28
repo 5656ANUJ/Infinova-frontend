041fa23ef531f324c73118361554d18871f3d30f
Shouryan Bharote <shouryanbharote888@gmail.com>
2025-10-28 22:55:22 +0530

    Added Scroll animation to the contact us btn of students and universities removed overflow x added animation to the homepage menu

--- src/pages/EduventuresForUniversities.jsx
+++ src/pages/EduventuresForUniversities.jsx
@@ -1,26 +1,32 @@
-import React from 'react'
-// import EduventuresForUniversitiesNavbar from '../../components/EduventuresForUniversitiesNavbar'
+import React, { useRef } from 'react'
 import EduventuresForUniversitiesNavbar from '../../components/EduventuresForUniversitiesNavbarNew'
-// import EduventuresForUniversitiesHeroSection from './EduventuresForUniversitiesHeroSection'
 import EduventuresUnversitiessHero from './EduventuresUniversititesHeroNew'
 import EduventuresUniversitiesContact from './EduventuresUniversitiesContact'
 import EduventuresCourses from './EduventuresCourses'
 import EduventuresTrainers from './EduventuresTrainers'
 import EduventuresTestimonials from './EduventuresTestimonials'
+
 const EduventuresForUniversities = () => {
+  const contactFormRef = useRef(null); // Create ref for contact form
+
+  // Function to scroll to contact form
+  const scrollToContactForm = () => {
+    contactFormRef.current?.scrollIntoView({ 
+      behavior: 'smooth',
+      block: 'start'
+    });
+  };
+
   return (
    <>
    <div className='w-[100vw] min-h-[100vh] '>
     <EduventuresForUniversitiesNavbar/>
-    {/* <EduventuresForUniversitiesHeroSection/> */}
-    <EduventuresUnversitiessHero/>
-    <EduventuresUniversitiesContact/>
+    <EduventuresUnversitiessHero scrollToContactForm={scrollToContactForm} />
+    <EduventuresUniversitiesContact ref={contactFormRef} />
     <EduventuresCourses/>
     <EduventuresTrainers/>
     <EduventuresTestimonials/>
    </div>
-      
-
    </>
   )
 }
