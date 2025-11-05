f469352c3acd8f499ad4dde2e3ac740abd67dda9
Shouryan Bharote <shouryanbharote888@gmail.com>
2025-11-05 09:37:37 +0530

    Added Student Trainer's section

--- src/pages/EduventuresForStudents.jsx
+++ src/pages/EduventuresForStudents.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useRef } from 'react'
 import Navbar from '../../components/EduventuresForStudentsNavbarNew'
 import EduventuresForStudentsHeroSection from '../../components/EduventuresForStudentsHeroSectionNew'
 import BookingFormForStudentsEduventures from '../../components/BookingFormForStudentsEduventures'
-import TrainerCard from '../components/TrainerCard/TrainerCard'
+// import TrainerCard from '../components/TrainerCard/TrainerCard'
 import StudentTrainersSection from '../components/StudentTrainerSection/StudentTrainerSection'
 import EduventuresTrainers from '../../components/EduventuresForStudentsTrainers'
 import EduventuresStudentsTestimonials from '../../components/EduventuresStudentsTestimonials'
@@ -39,7 +39,7 @@ const EduventuresForStudents = () => {
       <JobOrientedCoursesSection/>
       <SoftSkillsCoursesSection/>
       <EduventuresForStudentsSoftSkills/>
-      <TrainerCard image={"/dummy_trainer.jpg"} name={"Ajay"} title={"CEO"} verified={1} experience={"5 years"} expertise={"Web Developer"}description={"I am a Good Guy and i am a Placeholder for the trainers section card . I hope i dont cause trouble"}/>
+      {/* <TrainerCard image={"/dummy_trainer.jpg"} name={"Ajay"} title={"CEO"} verified={1} experience={"5 years"} expertise={"Web Developer"}description={"I am a Good Guy and i am a Placeholder for the trainers section card . I hope i dont cause trouble"}/> */}
       <StudentTrainersSection/>
       <EduventuresTrainers/>
       <EduventuresStudentsTestimonials/>
